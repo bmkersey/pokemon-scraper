e5a90c4357566efc3091fff9d3a4fa803e457c9b
Brendan <bmkersey@gmail.com>
2025-04-07 13:29:20 -0400

    refactored scraping to handle pokemon with only base forms vs those with other forms ie megas gigamax

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import time
+import json
 
 BASE_URL = "https://pokemondb.net"
 POKEDEX_URL = f"{BASE_URL}/pokedex/national"
@@ -29,25 +30,68 @@ def get_pokemon_list():
 def get_pokedex_text(url):
   r = requests.get(url)
   soup = BeautifulSoup(r.text, 'html.parser')
-  entries = {}
+  entries_by_form = {}
 
-  pokedex_rows = soup.select('table.vitals-table')[4].select('tr')
-  for row in pokedex_rows:
-    th = row.find('th')
-    td = row.find('td')
-    games = [span.text.strip() for span in th.find_all('span', class_='igame')]
-    grouped_games = "/".join(games)
-    entry = td.text.strip()
-    entries[grouped_games] = entry
-  return entries
+  dex_header = soup.find('h2', string='Pokédex entries')
+  if not dex_header:
+    return entries_by_form
+  
+
+  current = dex_header.find_next_sibling()
+  if current and current.name == 'div' and current.find('table'):
+    #only base forms
+    table = current.find('table')
+    entries = {}
+    for row in table.select('tr'):
+      th = row.find('th')
+      td = row.find('td')
+      if not th or not td:
+        continue
+      games = [span.text.strip() for span in th.select('span.igame')]
+      grouped_games = "/".join(games)
+      entries[grouped_games] = td.text.strip()
+    print(entries)
+    entries_by_form["default"] = entries
+
+  while current:
+    if current.name == 'h3':
+      form = current.text.strip()
+      table_div = current.find_next_sibling('div')
+      table = table_div.find('table') if table_div else None
+      if not table:
+        current = table_div.find_next_sibling() if table_div else current.find_next_sibling()
+        continue
+      
+      form_entries = {}
+      for row in table.select('tr'):
+        th = row.find('th')
+        td = row.find('td')
+        if not th or not td:
+          continue
+        games = [span.text.strip() for span in th.select('span.igame')]
+        grouped_names = "/".join(games)
+        entry = td.text.strip()
+        form_entries[grouped_names] = entry
+      entries_by_form[form] = form_entries
+      print(form_entries)
+    
+    if current.name == 'h2':
+      break
+    
+    current = current.find_next_sibling()
+
+  return entries_by_form
 
 def main():
   pokemon = get_pokemon_list()
   for mon in pokemon:
     print(f"Scraping data for {mon['name']}...")
     entries = get_pokedex_text(mon['details_url'])
-    print(entries)
+    mon['pokedex_entries'] = entries
     time.sleep(1)
+  
+  with open('pokedex.json', 'w') as f:
+    json.dump(pokemon, f, indent=2)
 
 
 
